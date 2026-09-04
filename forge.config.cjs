423a79f7c2cb7791b47e0ad44749705f0ef378e2
Carlos F. Martínez Sánchez <carlos.f.martinez.s+github@gmail.com>
2026-09-04 19:10:11 -0400

    build: add initial Electron Forge package configuration

---
+++ forge.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  packagerConfig: {
+    name: 'Personal Tax Ledger',
+    executableName: 'PersonalTaxLedger',
+    asar: false,
+    prune: false,
+    overwrite: true
+  },
+  makers: []
+};
